4e7c619db52b0088f82b964ad08be819c57cc569
Alexandre17araujo <bryansimones50@gmail.com>
2024-11-03 04:51:34 -0300

    Removido algumas linhas e adicionando um novo comentario

--- search.py
+++ search.py
@@ -36,21 +36,14 @@ from time import sleep
         
 
 # 4 - Forma dee clicar no item da imagem
-posicoes_encontradas = list()
 
-sleep(3)
-## Para cada vez que ele achar algo semelhante a imgaem pegue a posição e jogue dentro de "posicoes_encontradas".
-#for i in pyautogui.locateAllOnScreen('imagens/inserir.png'):
-#    posicoes_encontradas.append(i)
+## Cria uma lista armazenando os valores para todas as vezes em que achar o mesmo 
+## elemento que está dentro da imagem que, está armazenada em nossa máquina. 
 posicoes_encontradas = list(pyautogui.locateAllOnScreen('imagens/inserir.png'))
 
 ## Calcular os valores para que seja capiturado o centro do item encontrado. "Isso é de apenas um item".
-if posicoes_encontradas:
-    x_center = posicoes_encontradas[0].left + posicoes_encontradas[0].width / 2
-    y_center = posicoes_encontradas[0].top + posicoes_encontradas[0].height / 2
-    resultado = f'x={x_center}, y={y_center}' 
-else:
-    print('Nenhuma posição encontrada')
-
+x_center = posicoes_encontradas[0].left + posicoes_encontradas[0].width / 2
+y_center = posicoes_encontradas[0].top + posicoes_encontradas[0].height / 2
+resultado = f'x={x_center}, y={y_center}' 
 pyautogui.click(x=532.0, y=389.5)    
 
